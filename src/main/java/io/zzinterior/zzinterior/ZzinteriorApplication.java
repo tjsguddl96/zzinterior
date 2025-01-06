f7e46f719ca7e442648ad6dbf58c51db0446b969
김선형 <tjsguddl96@gmail.com>
2025-01-06 16:40:49 +0900

    first commit

---
+++ src/main/java/io/zzinterior/zzinterior/ZzinteriorApplication.java
@@ -0,0 +1,13 @@
+package io.zzinterior.zzinterior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzinteriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzinteriorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zzinterior/zzinterior/ZzinteriorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zzinterior.zzinterior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzinteriorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
